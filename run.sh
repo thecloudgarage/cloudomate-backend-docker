50c61762a97e3be995b35611b7f7f3bfb1833b19
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-01-21 17:53:13 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 PORT=3003
+containerversion=v1
 
 sudo docker run -t -d \
                 -v $(pwd)/cloudomate_scripts:/cloudomate_scripts \
                 -p $PORT:3003 \
-                --name="c5" \
+                --name="cloudomate$containerversion" \
                 thecloudgarage/cloudomate-backend-docker
