e7b37be3f800e14d9a950b0f17099bc922514e6b
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2022-01-21 14:28:51 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ PORT=3003
 sudo docker run -t -d \
                 -v $(pwd)/pyjojo_scripts:/pyjojo_scripts \
                 -p $PORT:3003 \
-                --name="pyjojo" \
+                --name="c3" \
                 thecloudgarage/cloudomate-backend-docker
